d67b0d2e8585d4649f85629f72168db97a4f1e2a
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2024-02-20 10:36:58 -0500

    Update test_sayari.py

--- src/sayari/test_sayari.py
+++ src/sayari/test_sayari.py
@@ -59,7 +59,7 @@ def test_entities(setup_connection):
 
     # test the GET variant of the search endpoint
     entity_search_get_results = client.search.search_entity_get(q=random_string)
-    assert entity_search_results.data == entity_search_get_results.data
+    assert len(entity_search_results.data) == len(entity_search_get_results.data)
     assert entity_search_results.size.count == entity_search_get_results.size.count
     assert entity_search_results.size.qualifier == entity_search_get_results.size.qualifier
 
