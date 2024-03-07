49b31523165e2aa4ef6a1ae62591bea280f2041a
sayari-robot <47253128+sayari-robot@users.noreply.github.com>
2024-03-07 08:38:06 -0600

    update ontology and risk from prod (#50)
    Co-authored-by: GitHub Actions Bot <>

--- src/sayari/python/resources/generated_types/types/entities.py
+++ src/sayari/python/resources/generated_types/types/entities.py
@@ -11,29 +11,14 @@ class Entities(str, enum.Enum):
     This represents which type of entity is being returned.
     """
 
-    PERSON = "person"
+    AIRCRAFT = "aircraft"
     """
-    A natural person (human being)
-    """
-
-    SECURITY = "security"
-    """
-    A tradable financial asset
+    An airplane, helicopter, or other vehicle that travels by flight
     """
 
-    TRADENAME = "tradename"
+    PROPERTY = "property"
     """
-    A discretely registered name used by a person or company not operating under its legal name. This includes doing-business-as (DBA) names, fictitious names, etc. in jurisdictions that treat them as registered objects distinct from the person/company using them.
-    """
-
-    COMPANY = "company"
-    """
-    A legal entity or organization
-    """
-
-    LEGAL_MATTER = "legal_matter"
-    """
-    A civil or criminal legal case or similar type of proceeding
+    Land, real estate, real property, or personal property not categorized under another entity type
     """
 
     UNKNOWN = "unknown"
@@ -41,19 +26,14 @@ class Entities(str, enum.Enum):
     An unknown placeholder entity. Rarely used. An unknown entity has insufficient information to be grouped by an existing entity type.
     """
 
-    SHIPMENT = "shipment"
+    LEGAL_MATTER = "legal_matter"
     """
-    A shipment between two entities
-    """
-
-    AIRCRAFT = "aircraft"
-    """
-    An airplane, helicopter, or other vehicle that travels by flight
+    A civil or criminal legal case or similar type of proceeding
     """
 
-    VESSEL = "vessel"
+    SECURITY = "security"
     """
-    A cargo ship, oil tanker, fishing trawler, or other type of watercraft
+    A tradable financial asset
     """
 
     GENERIC = "generic"
@@ -61,9 +41,29 @@ class Entities(str, enum.Enum):
     A generic placeholder entity. Rarely used. A generic entity typically does not fit any other entity type.
     """
 
-    PROPERTY = "property"
+    TRADENAME = "tradename"
     """
-    Land, real estate, real property, or personal property not categorized under another entity type
+    A discretely registered name used by a person or company not operating under its legal name. This includes doing-business-as (DBA) names, fictitious names, etc. in jurisdictions that treat them as registered objects distinct from the person/company using them.
+    """
+
+    VESSEL = "vessel"
+    """
+    A cargo ship, oil tanker, fishing trawler, or other type of watercraft
+    """
+
+    COMPANY = "company"
+    """
+    A legal entity or organization
+    """
+
+    PERSON = "person"
+    """
+    A natural person (human being)
+    """
+
+    SHIPMENT = "shipment"
+    """
+    A shipment between two entities
     """
 
     INTELLECTUAL_PROPERTY = "intellectual_property"
@@ -73,40 +73,40 @@ class Entities(str, enum.Enum):
 
     def visit(
         self,
-        person: typing.Callable[[], T_Result],
-        security: typing.Callable[[], T_Result],
-        tradename: typing.Callable[[], T_Result],
-        company: typing.Callable[[], T_Result],
-        legal_matter: typing.Callable[[], T_Result],
-        unknown: typing.Callable[[], T_Result],
-        shipment: typing.Callable[[], T_Result],
         aircraft: typing.Callable[[], T_Result],
-        vessel: typing.Callable[[], T_Result],
-        generic: typing.Callable[[], T_Result],
         property: typing.Callable[[], T_Result],
+        unknown: typing.Callable[[], T_Result],
+        legal_matter: typing.Callable[[], T_Result],
+        security: typing.Callable[[], T_Result],
+        generic: typing.Callable[[], T_Result],
+        tradename: typing.Callable[[], T_Result],
+        vessel: typing.Callable[[], T_Result],
+        company: typing.Callable[[], T_Result],
+        person: typing.Callable[[], T_Result],
+        shipment: typing.Callable[[], T_Result],
         intellectual_property: typing.Callable[[], T_Result],
     ) -> T_Result:
-        if self is Entities.PERSON:
-            return person()
-        if self is Entities.SECURITY:
-            return security()
-        if self is Entities.TRADENAME:
-            return tradename()
-        if self is Entities.COMPANY:
-            return company()
-        if self is Entities.LEGAL_MATTER:
-            return legal_matter()
-        if self is Entities.UNKNOWN:
-            return unknown()
-        if self is Entities.SHIPMENT:
-            return shipment()
         if self is Entities.AIRCRAFT:
             return aircraft()
-        if self is Entities.VESSEL:
-            return vessel()
-        if self is Entities.GENERIC:
-            return generic()
         if self is Entities.PROPERTY:
             return property()
+        if self is Entities.UNKNOWN:
+            return unknown()
+        if self is Entities.LEGAL_MATTER:
+            return legal_matter()
+        if self is Entities.SECURITY:
+            return security()
+        if self is Entities.GENERIC:
+            return generic()
+        if self is Entities.TRADENAME:
+            return tradename()
+        if self is Entities.VESSEL:
+            return vessel()
+        if self is Entities.COMPANY:
+            return company()
+        if self is Entities.PERSON:
+            return person()
+        if self is Entities.SHIPMENT:
+            return shipment()
         if self is Entities.INTELLECTUAL_PROPERTY:
             return intellectual_property()
