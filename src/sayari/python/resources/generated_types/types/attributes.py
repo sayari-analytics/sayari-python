95b9d4cae5093d94d129d0e812750d75f2eed7a6
sayari-robot <47253128+sayari-robot@users.noreply.github.com>
2024-04-18 10:39:49 -0500

    update ontology and risk from prod (#72)
    Co-authored-by: GitHub Actions Bot <>

--- src/sayari/python/resources/generated_types/types/attributes.py
+++ src/sayari/python/resources/generated_types/types/attributes.py
@@ -7,95 +7,95 @@ T_Result = typing.TypeVar("T_Result")
 
 
 class Attributes(str, enum.Enum):
-    BUSINESS_PURPOSE = "business_purpose"
-    ADDRESS = "address"
-    SHARES = "shares"
-    POSITION = "position"
-    TRANSLATED_NAME = "translated_name"
-    COUNTRY = "country"
-    NAME = "name"
-    RISK_INTELLIGENCE = "risk_intelligence"
-    IDENTIFIER = "identifier"
-    FINANCES = "finances"
-    PERSON_STATUS = "person_status"
-    MONETARY_VALUE = "monetary_value"
-    WEAK_IDENTIFIER = "weak_identifier"
-    ADDITIONAL_INFORMATION = "additional_information"
-    DATE_OF_BIRTH = "date_of_birth"
-    MEASUREMENT = "measurement"
-    GENERIC = "generic"
-    COMPANY_TYPE = "company_type"
-    GENDER = "gender"
-    FINANCIALS = "financials"
     STATUS = "status"
+    COMPANY_TYPE = "company_type"
+    NAME = "name"
+    WEAK_IDENTIFIER = "weak_identifier"
+    ADDRESS = "address"
+    FINANCES = "finances"
+    MONETARY_VALUE = "monetary_value"
+    BUSINESS_PURPOSE = "business_purpose"
+    IDENTIFIER = "identifier"
+    FINANCIALS = "financials"
+    MEASUREMENT = "measurement"
+    COUNTRY = "country"
+    RISK_INTELLIGENCE = "risk_intelligence"
+    TRANSLATED_NAME = "translated_name"
+    SHARES = "shares"
+    GENDER = "gender"
+    ADDITIONAL_INFORMATION = "additional_information"
     CONTACT = "contact"
+    POSITION = "position"
+    DATE_OF_BIRTH = "date_of_birth"
+    PERSON_STATUS = "person_status"
+    GENERIC = "generic"
 
     def visit(
         self,
-        business_purpose: typing.Callable[[], T_Result],
-        address: typing.Callable[[], T_Result],
-        shares: typing.Callable[[], T_Result],
-        position: typing.Callable[[], T_Result],
-        translated_name: typing.Callable[[], T_Result],
-        country: typing.Callable[[], T_Result],
-        name: typing.Callable[[], T_Result],
-        risk_intelligence: typing.Callable[[], T_Result],
-        identifier: typing.Callable[[], T_Result],
-        finances: typing.Callable[[], T_Result],
-        person_status: typing.Callable[[], T_Result],
-        monetary_value: typing.Callable[[], T_Result],
-        weak_identifier: typing.Callable[[], T_Result],
-        additional_information: typing.Callable[[], T_Result],
-        date_of_birth: typing.Callable[[], T_Result],
-        measurement: typing.Callable[[], T_Result],
-        generic: typing.Callable[[], T_Result],
-        company_type: typing.Callable[[], T_Result],
-        gender: typing.Callable[[], T_Result],
-        financials: typing.Callable[[], T_Result],
         status: typing.Callable[[], T_Result],
+        company_type: typing.Callable[[], T_Result],
+        name: typing.Callable[[], T_Result],
+        weak_identifier: typing.Callable[[], T_Result],
+        address: typing.Callable[[], T_Result],
+        finances: typing.Callable[[], T_Result],
+        monetary_value: typing.Callable[[], T_Result],
+        business_purpose: typing.Callable[[], T_Result],
+        identifier: typing.Callable[[], T_Result],
+        financials: typing.Callable[[], T_Result],
+        measurement: typing.Callable[[], T_Result],
+        country: typing.Callable[[], T_Result],
+        risk_intelligence: typing.Callable[[], T_Result],
+        translated_name: typing.Callable[[], T_Result],
+        shares: typing.Callable[[], T_Result],
+        gender: typing.Callable[[], T_Result],
+        additional_information: typing.Callable[[], T_Result],
         contact: typing.Callable[[], T_Result],
+        position: typing.Callable[[], T_Result],
+        date_of_birth: typing.Callable[[], T_Result],
+        person_status: typing.Callable[[], T_Result],
+        generic: typing.Callable[[], T_Result],
     ) -> T_Result:
-        if self is Attributes.BUSINESS_PURPOSE:
-            return business_purpose()
-        if self is Attributes.ADDRESS:
-            return address()
-        if self is Attributes.SHARES:
-            return shares()
-        if self is Attributes.POSITION:
-            return position()
-        if self is Attributes.TRANSLATED_NAME:
-            return translated_name()
-        if self is Attributes.COUNTRY:
-            return country()
-        if self is Attributes.NAME:
-            return name()
-        if self is Attributes.RISK_INTELLIGENCE:
-            return risk_intelligence()
-        if self is Attributes.IDENTIFIER:
-            return identifier()
-        if self is Attributes.FINANCES:
-            return finances()
-        if self is Attributes.PERSON_STATUS:
-            return person_status()
-        if self is Attributes.MONETARY_VALUE:
-            return monetary_value()
-        if self is Attributes.WEAK_IDENTIFIER:
-            return weak_identifier()
-        if self is Attributes.ADDITIONAL_INFORMATION:
-            return additional_information()
-        if self is Attributes.DATE_OF_BIRTH:
-            return date_of_birth()
-        if self is Attributes.MEASUREMENT:
-            return measurement()
-        if self is Attributes.GENERIC:
-            return generic()
-        if self is Attributes.COMPANY_TYPE:
-            return company_type()
-        if self is Attributes.GENDER:
-            return gender()
-        if self is Attributes.FINANCIALS:
-            return financials()
         if self is Attributes.STATUS:
             return status()
+        if self is Attributes.COMPANY_TYPE:
+            return company_type()
+        if self is Attributes.NAME:
+            return name()
+        if self is Attributes.WEAK_IDENTIFIER:
+            return weak_identifier()
+        if self is Attributes.ADDRESS:
+            return address()
+        if self is Attributes.FINANCES:
+            return finances()
+        if self is Attributes.MONETARY_VALUE:
+            return monetary_value()
+        if self is Attributes.BUSINESS_PURPOSE:
+            return business_purpose()
+        if self is Attributes.IDENTIFIER:
+            return identifier()
+        if self is Attributes.FINANCIALS:
+            return financials()
+        if self is Attributes.MEASUREMENT:
+            return measurement()
+        if self is Attributes.COUNTRY:
+            return country()
+        if self is Attributes.RISK_INTELLIGENCE:
+            return risk_intelligence()
+        if self is Attributes.TRANSLATED_NAME:
+            return translated_name()
+        if self is Attributes.SHARES:
+            return shares()
+        if self is Attributes.GENDER:
+            return gender()
+        if self is Attributes.ADDITIONAL_INFORMATION:
+            return additional_information()
         if self is Attributes.CONTACT:
             return contact()
+        if self is Attributes.POSITION:
+            return position()
+        if self is Attributes.DATE_OF_BIRTH:
+            return date_of_birth()
+        if self is Attributes.PERSON_STATUS:
+            return person_status()
+        if self is Attributes.GENERIC:
+            return generic()
--- src/sayari/python/resources/generated_types/types/entities.py
+++ src/sayari/python/resources/generated_types/types/entities.py
@@ -11,29 +11,9 @@ class Entities(str, enum.Enum):
     This represents which type of entity is being returned.
     """
 
-    INTELLECTUAL_PROPERTY = "intellectual_property"
-    """
-    A trademark, patent, copyright, or similar type of intangible property
-    """
-
     PROPERTY = "property"
     """
     Land, real estate, real property, or personal property not categorized under another entity type
-    """
-
-    COMPANY = "company"
-    """
-    A legal entity or organization
-    """
-
-    GENERIC = "generic"
-    """
-    A generic placeholder entity. Rarely used. A generic entity typically does not fit any other entity type.
-    """
-
-    LEGAL_MATTER = "legal_matter"
-    """
-    A civil or criminal legal case or similar type of proceeding
     """
 
     VESSEL = "vessel"
@@ -41,24 +21,9 @@ class Entities(str, enum.Enum):
     A cargo ship, oil tanker, fishing trawler, or other type of watercraft
     """
 
-    UNKNOWN = "unknown"
+    LEGAL_MATTER = "legal_matter"
     """
-    An unknown placeholder entity. Rarely used. An unknown entity has insufficient information to be grouped by an existing entity type.
-    """
-
-    SECURITY = "security"
-    """
-    A tradable financial asset
-    """
-
-    AIRCRAFT = "aircraft"
-    """
-    An airplane, helicopter, or other vehicle that travels by flight
-    """
-
-    PERSON = "person"
-    """
-    A natural person (human being)
+    A civil or criminal legal case or similar type of proceeding
     """
 
     SHIPMENT = "shipment"
@@ -66,47 +31,82 @@ class Entities(str, enum.Enum):
     A shipment between two entities
     """
 
+    SECURITY = "security"
+    """
+    A tradable financial asset
+    """
+
+    COMPANY = "company"
+    """
+    A legal entity or organization
+    """
+
+    PERSON = "person"
+    """
+    A natural person (human being)
+    """
+
+    UNKNOWN = "unknown"
+    """
+    An unknown placeholder entity. Rarely used. An unknown entity has insufficient information to be grouped by an existing entity type.
+    """
+
     TRADENAME = "tradename"
     """
     A discretely registered name used by a person or company not operating under its legal name. This includes doing-business-as (DBA) names, fictitious names, etc. in jurisdictions that treat them as registered objects distinct from the person/company using them.
     """
 
+    AIRCRAFT = "aircraft"
+    """
+    An airplane, helicopter, or other vehicle that travels by flight
+    """
+
+    INTELLECTUAL_PROPERTY = "intellectual_property"
+    """
+    A trademark, patent, copyright, or similar type of intangible property
+    """
+
+    GENERIC = "generic"
+    """
+    A generic placeholder entity. Rarely used. A generic entity typically does not fit any other entity type.
+    """
+
     def visit(
         self,
-        intellectual_property: typing.Callable[[], T_Result],
         property: typing.Callable[[], T_Result],
-        company: typing.Callable[[], T_Result],
-        generic: typing.Callable[[], T_Result],
-        legal_matter: typing.Callable[[], T_Result],
         vessel: typing.Callable[[], T_Result],
-        unknown: typing.Callable[[], T_Result],
-        security: typing.Callable[[], T_Result],
-        aircraft: typing.Callable[[], T_Result],
-        person: typing.Callable[[], T_Result],
+        legal_matter: typing.Callable[[], T_Result],
         shipment: typing.Callable[[], T_Result],
+        security: typing.Callable[[], T_Result],
+        company: typing.Callable[[], T_Result],
+        person: typing.Callable[[], T_Result],
+        unknown: typing.Callable[[], T_Result],
         tradename: typing.Callable[[], T_Result],
+        aircraft: typing.Callable[[], T_Result],
+        intellectual_property: typing.Callable[[], T_Result],
+        generic: typing.Callable[[], T_Result],
     ) -> T_Result:
-        if self is Entities.INTELLECTUAL_PROPERTY:
-            return intellectual_property()
         if self is Entities.PROPERTY:
             return property()
-        if self is Entities.COMPANY:
-            return company()
-        if self is Entities.GENERIC:
-            return generic()
-        if self is Entities.LEGAL_MATTER:
-            return legal_matter()
         if self is Entities.VESSEL:
             return vessel()
-        if self is Entities.UNKNOWN:
-            return unknown()
-        if self is Entities.SECURITY:
-            return security()
-        if self is Entities.AIRCRAFT:
-            return aircraft()
-        if self is Entities.PERSON:
-            return person()
+        if self is Entities.LEGAL_MATTER:
+            return legal_matter()
         if self is Entities.SHIPMENT:
             return shipment()
+        if self is Entities.SECURITY:
+            return security()
+        if self is Entities.COMPANY:
+            return company()
+        if self is Entities.PERSON:
+            return person()
+        if self is Entities.UNKNOWN:
+            return unknown()
         if self is Entities.TRADENAME:
             return tradename()
+        if self is Entities.AIRCRAFT:
+            return aircraft()
+        if self is Entities.INTELLECTUAL_PROPERTY:
+            return intellectual_property()
+        if self is Entities.GENERIC:
+            return generic()
