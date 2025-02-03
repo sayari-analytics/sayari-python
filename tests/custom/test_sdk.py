d1f74e91f375957525fcae838c13c82b24037d07
David Konigsberg <72822263+davidkonigsberg@users.noreply.github.com>
2025-02-03 08:20:47 -0500

    reverse shortest path

--- tests/custom/test_sdk.py
+++ tests/custom/test_sdk.py
@@ -245,7 +245,8 @@ def test_ownership_traversal(setup_connection):
     """
 
     # shortest path
-    shortest_path = client.traversal.shortest_path(entities=[entity.id, ubo_id])
+    # TODO: revert reversal of query once bug is fixed
+    shortest_path = client.traversal.shortest_path(entities=[ubo_id, entity.id])
     assert len(shortest_path.data[0].path) > 0
 
 # TODO: figure out good test for watchlist traversal
