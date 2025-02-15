d4983682028b7f9ce65d6b3a487113da3d499be9
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-02-15 00:34:48 +0000

    SDK regeneration

--- src/sayari/core/client_wrapper.py
+++ src/sayari/core/client_wrapper.py
@@ -22,7 +22,7 @@ class BaseClientWrapper:
         headers: typing.Dict[str, str] = {
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "sayari",
-            "X-Fern-SDK-Version": "0.1.36",
+            "X-Fern-SDK-Version": "0.1.37",
         }
         token = self._get_token()
         if token is not None:
